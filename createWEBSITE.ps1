4773044256537898cb9e1a91f0f9b804698fbdc4
romaric0105 <sarmiento.romaric01@gmail.com>
2024-09-09 16:15:14 +0800

    Update createWEBSITE.ps1

--- createWEBSITE.ps1
+++ createWEBSITE.ps1
@@ -1,3 +1,3 @@
 ﻿#creating web apps
-New-Website -name "ngcp website" -hostheader "www.ngcpM.com" -physicalpath "c:\webs\datingbiz"
+New-Website -name "ccna12b website" -hostheader "www.ccna12b.com" -physicalpath "d:\webs\appgbiz"
 						   
